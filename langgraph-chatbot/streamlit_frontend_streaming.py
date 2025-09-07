a673f4e3acb7d47e4baf85e68f506b1790d1eeea
himanshu <student030205@gmail.com>
2025-09-07 19:14:10 +0530

    no changes

--- langgraph-chatbot/streamlit_frontend_streaming.py
+++ langgraph-chatbot/streamlit_frontend_streaming.py
@@ -29,11 +29,11 @@ if user_input:
     with st.chat_message("assistant"):
     
         ai_message = st.write_stream(
-            message_chunk.content for message_chunk, metadata in chatbot.stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream( # type: ignore
             {'messages': [HumanMessage(content=user_input)]},
             config = {'configurable': {'thread_id': 'thread-1'}},
             stream_mode = 'messages'
-            )
-        )
+            ) 
+        ) 
 
     st.session_state['message_history'].append({"role": "assistant", "content": ai_message})

7d4ed0669c44bdfffc42ec3e6b83a3860853ad85
himanshu <student030205@gmail.com>
2025-09-07 18:44:37 +0530

    beta chatbot using llm version 1

---
+++ langgraph-chatbot/langgraph_backend.py
@@ -0,0 +1,48 @@
+
+
+import os 
+from dotenv import load_dotenv
+from langgraph.graph import StateGraph,START, END
+from typing import TypedDict, Literal, Annotated
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import BaseMessage, HumanMessage
+import pprint
+from langgraph.checkpoint.memory import MemorySaver
+
+load_dotenv()  
+api_key = os.getenv("GOOGLE_API_KEY")
+if not api_key:
+    raise ValueError("GOOGLE_API_KEY not found in environment variables")
+
+from langgraph.graph.message import add_messages
+
+class ChatState(TypedDict):
+    
+    messages: Annotated[list[BaseMessage], add_messages]
+
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+
+def chat_node(state: ChatState) -> ChatState:
+
+    # take user input from the state
+    messages = state['messages']
+
+    # send to llm
+    response = llm.invoke(messages)
+
+    # response store state
+    return {'messages': [response]}
+
+checkpointer = MemorySaver()
+
+graph = StateGraph(ChatState)
+
+# add nodes
+graph.add_node('chat_node',chat_node)
+
+# add edges
+graph.add_edge(START,'chat_node')
+graph.add_edge('chat_node',END)
+
+# compile
+chatbot = graph.compile(checkpointer=checkpointer)
---
+++ langgraph-chatbot/streamlit_frontend.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from langgraph_backend import chatbot
+from  langchain_core.messages import BaseMessage, HumanMessage, AIMessage
+
+# threads
+CONFIG = {'configurable': {'thread_id': 'thread-1'}}
+
+# for preventing refreshing the chat history
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+
+for message in st.session_state['message_history']:
+    with st.chat_message(message["role"]):
+        st.text(message["content"])
+
+user_input = st.chat_input("Enter your text here:")
+
+# running the chatbot
+if user_input:
+
+    # first add the message to the history
+    st.session_state['message_history'].append({"role": "user", "content": user_input})
+    with st.chat_message("user"):
+        st.text(user_input)
+
+    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG) #type: ignore 
+    ai_message = response['messages'][-1].content
+    # first add the message to the history
+    st.session_state['message_history'].append({"role": "assistant", "content": ai_message})
+    with st.chat_message("assistant"):
+        st.text(ai_message)
